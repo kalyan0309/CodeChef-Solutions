18a2cb39638509101d52816c6bf7cade73be9997
CH. LALITHA KALYAN <77020331+kalyan0309@users.noreply.github.com>
2022-10-24 22:42:57 +0530

    Create Audible Range.py

---
+++ Audible Range.py
@@ -0,0 +1,9 @@
+for _ in range(int(input())):
+    n = int(input())
+    if n>=67:
+        if n<=45000:
+            print("YES")
+        else:
+            print("NO")
+    else:
+        print("NO")
